4afeed6a1392d99093152a9044bb61e275d2782b
dulguun <dulguunb@protonmail.com>
2019-02-09 21:02:18 +0100

    seperated into dll_util.c

--- ApplnIntegration/dll.c
+++ ApplnIntegration/dll.c
@@ -37,56 +37,5 @@ add_data_to_dll (dll_t *dll, void *app_data){
     dll->head = dll_new_node;
     return 0;
 }
-int remove_one_node(dll_node_t** head,dll_node_t* to_be_removed)
-{
-    if(!head || !to_be_removed) 
-        return -1;
-    
-    if(*head == to_be_removed){
-        *head = to_be_removed->next;
-        return 0;
-    }
-    if(to_be_removed->next != NULL){
-        to_be_removed->next->prev = to_be_removed->prev; 
-        if(to_be_removed->prev != NULL){
-            to_be_removed->prev->next = to_be_removed->next;
-        }
-        free(to_be_removed);
-        return 0;
-    } 
-    if(to_be_removed->prev != NULL){
-        to_be_removed->prev->next = to_be_removed->next;
-        free(to_be_removed);
-        return 0;
-    }
-    return -1;
-}
-/*More functions*/
-int  /*return 0 on success and -1 on failure*/
-remove_data_from_dll_by_data_ptr (dll_t *dll, void *data)
-{
-    if(!dll || !data) return -1;
-    dll_node_t *current = dll->head;
-    while(current!=NULL){
-        if(current->data == data){
-            return remove_one_node(&dll->head,current);
-        }
-        else{
-            current = current->next;
-        }
-    }
-    return -1;
-}
 
-void
-drain_dll (dll_t *dll)
-{
-    dll_node_t *current = dll->head;
-    dll_node_t *next;
-    while(current!=NULL){
-        next = current->next;
-        free(current);
-        current = next;
-    }
-    dll->head = NULL;
-}
+/*More functions*/
--- ApplnIntegration/dll_util.c
+++ ApplnIntegration/dll_util.c
@@ -3,23 +3,68 @@
 #include <memory.h>
 
 /*More functions*/
-int  /*return 0 on success and -1 on failure*/
-remove_data_from_dll_by_data_ptr (dll_t *dll, void *data){
 
-    return 0;
+int remove_one_node(dll_node_t** head,dll_node_t* to_be_removed)
+{
+    if(!head || !to_be_removed) 
+        return -1;
+    
+    if(*head == to_be_removed){
+        *head = to_be_removed->next;
+        return 0;
+    }
+    if(to_be_removed->next != NULL){
+        to_be_removed->next->prev = to_be_removed->prev; 
+        if(to_be_removed->prev != NULL){
+            to_be_removed->prev->next = to_be_removed->next;
+        }
+        free(to_be_removed);
+        return 0;
+    } 
+    if(to_be_removed->prev != NULL){
+        to_be_removed->prev->next = to_be_removed->next;
+        free(to_be_removed);
+        return 0;
+    }
+    return -1;
+}
+
+  /*return 0 on success and -1 on failure*/
+int  /*return 0 on success and -1 on failure*/
+remove_data_from_dll_by_data_ptr (dll_t *dll, void *data)
+{
+    if(!dll || !data) return -1;
+    dll_node_t *current = dll->head;
+    while(current!=NULL){
+        if(current->data == data){
+            return remove_one_node(&dll->head,current);
+        }
+        else{
+            current = current->next;
+        }
+    }
+    return -1;
 }
 
 /*return 0 if empty, -1 if not empty*/
 int
 is_dll_empty (dll_t *dll){
-
-    return 0;
+    if( dll->head != NULL ) return 0;
+    return -1;
 }
 
 /* delete all nodes from a dll, but do not free appln data*/
+
 void
-drain_dll (dll_t *dll){
-
-
+drain_dll (dll_t *dll)
+{
+    dll_node_t *current = dll->head;
+    dll_node_t *next;
+    while(current!=NULL){
+        next = current->next;
+        free(current);
+        current = next;
+    }
+    dll->head = NULL;
 }
 
